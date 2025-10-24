92f3987d01ace6a36e4151837112271e59f89153
ItayVolk <ivolk@scu.edu>
2025-10-24 13:43:11 -0700

    Implemented life

--- life.cxx
+++ life.cxx
@@ -8,11 +8,31 @@
 #include "rng.h"
 #include "dna.h"
 #include "life.h"
+#include <algorithm>
 
 namespace csen79 {
 
 // student to rewrite both
-Life Life::cross(const Life& other) const { Life child; return child; }
-const unsigned int Life::dnaMatch(int c, const DNA& other) const {return 0;}
+Life Life::cross(const Life& other) const {
+    Life child;
+    child.generation = std::max(generation, other.generation)+1;
+    for (int i = 0; i < NChromo; i++) {
+        child.dna[i] = DNA(0);
+    }
+    for (int i = 0; i < NChromo/2; i++) {
+        int index;
+        do {
+            index = rand_int(0, NChromo-1);
+        } while (child.dna[index].count());
+        child.dna[index] = dna[index];
+    }
+    for (int i = 0; i < NChromo; i++) {
+        if (!child.dna[i].count()) {
+            child.dna[i] = other.dna[i];
+        }
+    }
+    return child;
+}
+const unsigned int Life::dnaMatch(int c, const DNA& other) const {return dna[c].matchDNA(other);}
 
 }   // namespace csen79
