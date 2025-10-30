6fbb880e0c3ee3fd73f487da32d79362ef231e9f
abhinav ala <aala@linux10612.dc.engr.scu.edu>
2025-10-30 13:21:26 -0700

    Implement DNA class with all required methods
    - Implemented DNA(unsigned int) constructor with proper masking
    - Implemented getCode() to retrieve individual bits
    - Implemented setCode() to set individual bits
    - Implemented size() to return DNA length (16)
    - Implemented count() to count set bits
    - Implemented matchDNA() using XOR to count matching bits
    - Added comments explaining each implementation
    - All methods tested and verified working with main program

--- dna.cxx
+++ dna.cxx
@@ -9,11 +9,34 @@
 namespace csen79 {
 
 // Student to implement all of these member functions
-DNA::DNA(unsigned int v) {}
-bool DNA::getCode(const int i) const { return true; }
-void DNA::setCode(const int i, bool v) {}
-size_t DNA::size() const {return 0;}
-size_t DNA::count() const {return 0;}
-unsigned int DNA::matchDNA(const DNA& other) const { return 0; }
+DNA::DNA(unsigned int v) {
+    // Initialize the bitset with the lower bits of v
+    codes = static_cast<Gene>(v & mask);
+}
+
+bool DNA::getCode(const int i) const {
+    // Return the bit at position i
+    return codes.test(i);
+}
+
+void DNA::setCode(const int i, bool v) {
+    // Set the bit at position i to value v
+    codes.set(i, v);
+}
+
+size_t DNA::size() const {
+    // Return the length of the DNA aka number of codes
+    return codes.size();
+}
+
+size_t DNA::count() const {
+    // Return the count of bits that are set to 1
+    return codes.count();
+}
+
+unsigned int DNA::matchDNA(const DNA& other) const {
+    // Return how many bits match between this DNA and the other DNA using xor
+    return (codes ^ other.codes).flip().count();
+}
 
 }   // namespace csen79
